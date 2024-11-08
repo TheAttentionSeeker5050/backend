169bf868314816ba666c04dfcfe2eec0d245a3ae
Nicolas Castellano <nicolas.castellano.6@hotmail.com>
2024-11-08 14:12:37 -0400

    built an rest microservice out of the model implementation

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import json
 # Load .env file, this holds the secret keys and other important variables
 load_dotenv()
 
+# The FastAPI server that will be used to host the chatbot
+from fastapi import FastAPI
+from langchain_core.prompts import ChatPromptTemplate
+from langserve import add_routes
+
 # The OpenAI model that will be used to generate responses
 from langchain_openai import ChatOpenAI
 
@@ -16,21 +21,54 @@ from langchain_core.output_parsers import StrOutputParser
 
 model = ChatOpenAI(model="gpt-4-turbo", api_key=os.getenv("OPENAI_API_KEY"))
 
-messages = [
-    SystemMessage(content="Return a json array format response for the following query with the keys being name, content, source and date:"),
-    HumanMessage(content="Show me the top 3 news of the last 24h, my location is nova scotia canada"),
-]
+# Make the templates for requests
+system_template = "Return a json array format response for the following query with the keys being name, content, source and date:"
+prompt_template = ChatPromptTemplate.from_messages([
+    ('system', system_template),
+    ('user', '{text}')
+])
 
-result = model.invoke(messages)
+# 3 news in Nova Scotia, Canada
+user_template_local = "Show me the top 3 news of the last 24h, my location is nova scotia canada"
+
+
 
 # a method to strip the json top and bottom headers from the response
 def strip_json_headers(response):
     return response.strip("```json").strip("```")
 
-# Try to parse the result into a JSON object
-try:
-    parsedResult = json.loads(strip_json_headers(result.content))  # Parse the response content into a structured JSON object
-    print("Parsed Result: ", parsedResult)
-except json.JSONDecodeError as e:
-    print("Error parsing JSON")
+# Method to parse the result into a JSON object
+def parse_result_to_json(result):
+    try:
+        return json.loads(strip_json_headers(result))  # Parse the response content into a structured JSON object
+        
+    except json.JSONDecodeError as e:
+        print("Error parsing JSON")
+        return None
+    
+# Create output parser
+parser = StrOutputParser()
+
+# Create chain
+chain = prompt_template | model | parser | parse_result_to_json
+
+# Make the FastAPI REST API
+app = FastAPI(
+  title="Langchain 3 news API",
+  version="1.0",
+  description="A simple API server using LangChain's Runnable interfaces for the web application 3 news, which 3 news with ai, unbiased and fast.",
+)
+
+# Adding routes
+add_routes(
+    app,
+    chain,
+    path="/news",
+)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
+
 
